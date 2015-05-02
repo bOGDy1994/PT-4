2fe06e8685e0c9a58669d52d3cc27dff46134100
bOGDy1994 <bogdy.maxim94@yahoo.com>
2015-05-02 16:47:42 +0300

    add hashCode and equals method

--- src/model/Person.java
+++ src/model/Person.java
@@ -16,6 +16,36 @@ public class Person {
 		email = inEmail;
 	}
 	
+	private int hash(String name)
+	{
+		int Sum = 0;
+		for(int i = 0; i<name.length(); i++)
+			Sum = Sum + name.charAt(i);
+		return Sum;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return 37 * age + 13 * hash(name);
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if(o == null) return false;
+		if(!(o instanceof Person)) return false;
+		if(hashCode() == o.hashCode())
+		{
+			Person comp = (Person)o;
+			if(age!=comp.getAge()) return false;
+			if(!name.equals(comp.getName())) return false;
+			if(!phonenumber.equals(comp.getPhonenumber())) return false;
+			if(!email.equals(comp.getEmail())) return false;
+		}
+		return true;
+	}
+	
 	public int getID()
 	{
 		return id;
