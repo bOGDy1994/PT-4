29850766dda33324c4234d26a7d01b637e4aacea
bOGDy1994 <bogdy.maxim94@yahoo.com>
2015-05-02 16:45:06 +0300

    add interface methods and design by contract

--- src/logic/BankProc.java
+++ src/logic/BankProc.java
@@ -1,9 +1,59 @@
 package src.logic;
 
+import src.model.Account;
+import src.model.Person;
+import src.model.Types;
+
 public interface BankProc {
 
-	public void addAccount();
-	public void removeAccount();
-	
-	
+	/**
+	 * 
+	 * @pre p.getID()>=0 
+	 * @pre p.getAge()>=18 
+	 * @pre !p.getName().equals("") 
+	 * @pre p.getPhonenumber().length == 10 
+	 * @pre p.getEmail().equals(*@*.*)
+	 * @pre inMoney>0
+	 * @pre t!=NULL
+	 * @pre  Entry<p, a> entry ==NULL
+	 * @post Entry<p, a> entry !=NULL
+	 */
+	public void addAccount(Person p, Account a, double inMoney, Types.accountTypes t);
+	/**
+	 * 
+	 * @pre p.getID()>=0 
+	 * @pre p.getAge()>=18 
+	 * @pre !p.getName().equals("") 
+	 * @pre p.getPhonenumber().length == 10 
+	 * @pre p.getEmail().equals(*@*.*)
+	 * @pre inMoney>0
+	 * @pre Entry<p, a> entry !=NULL
+	 * @post Entry<p, a> entry ==NULL
+	 */
+	public void removeAccount(Person p, Account a);
+	/**
+	 * 
+	 * @pre p.getID()>=0 
+	 * @pre p.getAge()>=18 
+	 * @pre !p.getName().equals("") 
+	 * @pre p.getPhonenumber().length == 10 
+	 * @pre p.getEmail().equals(*@*.*)
+	 * @pre inMoney>0
+	 * @pre t!=NULL
+	 * @pre Entry<p, a> entry == NULL
+	 * @post Entry<Person, Account> entry !=NULL
+	 */
+	public void addPerson(Person p, Account a, double inMoney, Types.accountTypes t);
+	/**
+	 * 
+	 * @pre p.getID()>=0 
+	 * @pre p.getAge()>=18 
+	 * @pre !p.getName().equals("") 
+	 * @pre p.getPhonenumber().length == 10 
+	 * @pre p.getEmail().equals(*@*.*)
+	 * @pre Entry<p, a> entry !=NULL
+	 * @post foreach Entry<p, a> entry in Entry
+	 * 			entry == NULL
+	 */
+	public void removePerson(Person p);
 }
